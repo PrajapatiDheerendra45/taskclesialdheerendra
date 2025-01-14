90b388895149eeb7bba780051d5929609eb3635d
prajapatidheerendra45 <Praja@123>
2025-01-14 16:11:31 +0530

    dh final commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Home from './components/Home'
 import ProductCard from './components/ProductCard'
 import ContactForm from './components/ContactForm'
 import ProductCategories from './components/ProductCategories'
+import HeroSection from './components/HeroSection'
 
 function App() {
   return (
     <>
     <Home/>
+    <HeroSection/>
     <ProductCategories/>
     <ProductCard/>
     <ContactForm/>
